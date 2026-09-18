b3ef8db1e3260a90420511f3343ef6dd90768467
ciresmatthew <cires.matthew@student.ccm.edu>
2026-09-18 10:55:19 -0400

    Add basic arithmetic operations program- finished part 1

---
+++ arithmetic_operations.py
@@ -0,0 +1,11 @@
+#Matthew Cires
+#CMP 131
+#Week 4 
+#Lab 2 
+#Basic Arithmetic Operations
+#9/18/26
+print("==================================")
+print("Basic Arithmetic Operations Program")
+print("==================================")
+First_number=input("how old is your brother:")
+Second_number=input("how old are you:")
