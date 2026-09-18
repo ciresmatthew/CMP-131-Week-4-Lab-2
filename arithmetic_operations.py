d7356497db13f610325a45128445a3c0f182acba
ciresmatthew <cires.matthew@student.ccm.edu>
2026-09-18 11:54:43 -0400

    Refactor arithmetic_operations.py for enhanced functionality- finished
    Updated the arithmetic operations program to include additional operations and improved input handling.

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -7,5 +7,17 @@
 print("==================================")
 print("Basic Arithmetic Operations Program")
 print("==================================")
-First_number=input("how old is your brother:")
-Second_number=input("how old are you:")
+first_number=float(input("first number:"))
+second_number=float(input("second number:"))
+total= first_number + second_number
+print(f"total: {total:.2f}")
+subtraction_result= first_number - second_number
+print(f"subtraction_result:{subtraction_result:.2f}")
+multiplication_result= first_number*second_number
+print(f"multiplication_result:{multiplication_result:.2f}")
+division_result= first_number/ second_number
+print(f"division_result:{division_result:.2f}")
+power= first_number **second_number
+print(f"power:{power:.2f}") 
+average = (first_number + second_number)/2
+print(f"average:{average:.2f}")
